97d1973517e544bba5eae6a3bc492c446af027cb
Ayam Srivastava <ayam.24b1531071@abes.ac.in>
2026-07-24 10:24:44 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-let a = 10;
-let b = 20;
+// let a = 10;
+// let b = 20;
 
-console.log(a + b);
+// console.log(a + b);
+
+function login (msg,error)
+{
+    if (error){
+        console.log("Error is "+error);
+
+    }
+    else{
+        console.log("Welcome"+msg);
+    }
+}
+function loginVerification(username,password,clbk){
+if (username =="ptomer40" && password=="123456"){
+    clbk ("Success",null);
+}else{
+    clbk (null,"username or password is incorrect");
+
+}}
+loginVerification("ptomer40","123456",login);
