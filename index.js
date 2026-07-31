fb5be4ed0f3b87908ccdc0da9c812858af319cd4
Ayam Srivastava <ayam.24b1531071@abes.ac.in>
2026-07-31 10:27:40 +0530

    msg

--- index.js
+++ index.js
@@ -3,21 +3,76 @@
 
 // console.log(a + b);
 
-function login (msg,error)
-{
-    if (error){
-        console.log("Error is "+error);
+// function login (msg,error)
+// {
+//     if (error){
+//         console.log("Error is "+error);
 
+//     }
+//     else{
+//         console.log("Welcome"+msg);
+//     }
+// }
+// function loginVerification(username,password,clbk){
+// if (username =="ptomer40" && password=="123456"){
+//     clbk ("Success",null);
+// }else{
+//     clbk (null,"username or password is incorrect");
+
+// }}
+// loginVerification("ptomer40","123456",login);
+// console.log("first")
+// // setTimeout(()=>(console.log("second")),1000) //it gives time gives others chance to execute first
+// for(i=1;i<10000;i++){
+//     console.log("second")
+// }
+// console.log("third")
+// setTimeout(() => {
+//     console.log("1")
+//     setTimeout(()=>{console.log("2")
+//         setTimeout(() => {console.log("3")
+//             setTimeout(() => {console.log("4")
+//                 setTimeout(() => {console.log("5")
+//                     setTimeout(() => {console.log("6")
+//                         setTimeout(() => {console.log("7")
+//                             setTimeout(() => {console.log("8")
+                                
+//                             }, 1000);
+//                         }, 1000);
+//                     }, 1000);
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     },1000);
+// }, 1000);
+
+//promises
+const myPromise=new Promise((resolve, reject) => {
+    username="ptomer40"
+    password="1234"
+    if(username=="ptomer" && password=="1234"){
+        resolve("sucess")
     }
     else{
-        console.log("Welcome"+msg);
+        resolve("username or password is incorrect")
     }
-}
-function loginVerification(username,password,clbk){
-if (username =="ptomer40" && password=="123456"){
-    clbk ("Success",null);
-}else{
-    clbk (null,"username or password is incorrect");
+})
 
-}}
-loginVerification("ptomer40","123456",login);
+myPromise.then((msg)=>{
+    console.log(msg)
+}).catch((msg)=>{
+    console.log(msg)
+}).finally(()=>{
+    console.log("all thr resouses have been closed /memory released")
+})
+async function handlelogin(){
+    try{
+        await myPromise}
+        catch(e){
+            console.log(e)
+        }
+        finally{
+            console.log("all thr resouses have been closed /memory released")
+        }
+    }
+    
